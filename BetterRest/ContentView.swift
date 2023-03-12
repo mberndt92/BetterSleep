058c8854a2d5cc5d95baebc44fdb0620cc0ce5af
mberndt92 <maximilian_berndt@icloud.com>
2023-03-13 08:51:16 +0900

    Use Sections instead of VStack

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -28,33 +28,34 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
+                Section {
                     DatePicker(
                         "Please enter a time",
                         selection: $wakeUp,
                         displayedComponents: .hourAndMinute
                     )
                     .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                Section {
                     Stepper(
                         "\(sleepAmount.formatted()) hours",
                         value: $sleepAmount,
                         in: 4...12, step: 0.25
                     )
+                } header: {
+                    Text("Desired amount of sleep")
                 }
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
+
+                Section {
                     Stepper(
                         "^[\(coffeeAmount) cup](inflect: true)",
                         value: $coffeeAmount,
                         in: 1...20
                     )
+                } header: {
+                    Text("Daily coffee intake")
                 }
             }
             .navigationTitle("BetterRest")
