325a7002dbcc5a78c5c9ea4cfe9d729223a3e26c
mberndt92 <maximilian_berndt@icloud.com>
2023-03-12 12:04:42 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Maximilian Berndt on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
